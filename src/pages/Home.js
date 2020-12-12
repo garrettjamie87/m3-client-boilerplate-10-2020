59469859b78aff9ebef903c517fa6f64d8a02f91
Jamie Garrett <garrettjamie87@googlemail.com>
2020-12-12 18:30:06 +0000

    change home page style

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,28 +1,30 @@
-import React from 'react';
-import './Home.css';
+import React from "react";
+import "./home.css";
 import Button from "react-bootstrap/Button";
+import logo from "../images/big-mouth-logo.png";
 
 function Home() {
   return (
-    <div className = 'home-div'> 
-       <img className ='logo' src= "../../images/logo3.png" alt="logo"/>
-       <div className= 'strap-line'>
-       <h1 >LANGUAGE EXCHANGE MADE EASY</h1>
+    <div className="home">
+      <img className="logo" src={logo} alt="Barcelona Bigmouths" />
+      <h1>Language Exchange made easy</h1>
 
-       <div className = "buttons"  >
-       <Button className = "signup-button" href = '/signup' size="lg" block>
-        SIGNUP 
-      </Button>
-      
-      <Button className = "login-button" href = '/login' variant="primary" size="lg" block>
-       LOGIN  
-      </Button>
-      
-      </div>
-     
+      <div className="buttons">
+        <Button
+          className="login-button"
+          href="/login"
+          variant="primary"
+          size="lg"
+          block
+        >
+          LOGIN &#x2192;
+        </Button>
+        <Button className="signup-button" href="/signup" size="lg" block>
+          SIGNUP &#x2192;
+        </Button>
       </div>
     </div>
-  )
+  );
 }
 
 export default Home;
